2ce9cf1e5343faaea7b8a4b15de98587c6838d29
Vid Beranic <vidberanic@gmail.com>
2024-12-02 17:30:38 +0100

    responsive task 1

--- frontend/src/Components/PokemonInfo.tsx
+++ frontend/src/Components/PokemonInfo.tsx
@@ -100,54 +100,54 @@ const PokemonInfo: React.FC<PokemonInfoParams> = ({selectedPokemon}) => {
             <div className=''>
               {pokemonInfo != EmptyPokemonLng ? (
                 <div className='p-8 grid grid-cols-8 '>
-                  {/* weight and height */}
-                  <div className="col-span-1 flex justify-between relative">
-                    <div className='flex items-center text-3xl'>
-                      <p>{pokemonInfo.height} Cm</p>
+                  <div className='lg:flex md:col-span-4 col-span-8 relative'>
+                    {/* weight and height */}
+                    <div className="flex justify-between relative lg:w-1/4 md:items-center items-center">
+                      <div className='flex lg:items-center sm:items-center text-3xl'>
+                        <p>{pokemonInfo.height} Cm</p>
+                      </div>
+                      <div className='lg:absolute right-2 top-2'>
+                        <TextInLogo textInMiddle={pokemonInfo.weight.toString() + " Kg"} baseSize={100} />
+                      </div>
                     </div>
-                    <div className='absolute right-2 top-2'>
-                      <TextInLogo textInMiddle={pokemonInfo.weight.toString() + " Kg"} baseSize={100} />
+
+                      {/* image and sprites */}
+                      <div className=' lg:mt-12 pt-2 md:flex '>
+                        {/* big img */}
+                        <div className='xl:w-96 xl:h-96 md:w-80 md:h-80 sm:w-60 sm:h-60 w-40 h-40 sm:mx-auto ml-2  rounded-full border-2 border-primaryBlue p-8 nonSelectable overflow-hidden items-center flex'>
+                          <img src={pokemonInfo.image} alt="Pokemon Artwork " className='object-scale-down'/>
+                        </div>
+
+                        {/* other sprites */}
+                        {(pokemonIsOfficial && pokemonInfo.sprites.length > 0) &&
+                          <div className='absolute xl:bottom-2 lg:bottom-14 md:bottom-0 bottom-0 md:right-0 right-12 flex items-end '>
+                            <IoCaretBackOutline onClick={prevImage} size={30} className='arrowIcons'/>
+                            <div className=''>
+                              <div className='xl:w-32 xl:h-32 lg:w-28 lg:h-28 sm:w-20 sm:h-20 w-10 h-10 mx-auto rounded-full border-2 border-primaryBlue p-2 flex justify-center items-center bg-primaryWhite '>
+                                <img 
+                                  src={pokemonInfo.sprites[shownImageIdx].sprite && pokemonInfo.sprites[shownImageIdx].sprite} 
+                                  alt="Pokemon sprite" 
+                                  className='object-scale-down min-w-full min-h-full rounded-full nonSelectable' 
+                                  />
+                              </div>
+                              <p className='nonSelectable sm:text-base text-sm'>{pokemonInfo.sprites[shownImageIdx].gen}</p>
+                            </div>
+                            <IoCaretForwardOutline onClick={nextImage} size={30} className='arrowIcons'/>
+                          </div>
+                        }
                     </div>
                   </div>
-                  {/* image and sprites */}
-                    <div className='col-span-3 relative'>
-                        {/* big img */}
-                      <div className='w-96 h-96 rounded-full border-2 border-primaryBlue p-8 nonSelectable overflow-hidden items-center flex'>
-                        <img src={pokemonInfo.image} alt="Pokemon Artwork " className='object-scale-down'/>
-                      </div>
-
-                      {/* other sprites */}
-                      {(pokemonIsOfficial && pokemonInfo.sprites.length > 0) &&
-                        <div className='absolute bottom-2 right-2 flex items-end'>
-                          <IoCaretBackOutline onClick={prevImage} size={30} className='hover:scale-125 duration-200 transition-all'/>
-                          <div>
-                            <div className='w-32 h-32 rounded-full border-2 border-primaryBlue p-2 flex justify-center items-center bg-primaryWhite '>
-                              <img 
-                                src={pokemonInfo.sprites[shownImageIdx].sprite && pokemonInfo.sprites[shownImageIdx].sprite} 
-                                alt="Pokemon sprite" 
-                                className='object-scale-down min-w-full min-h-full rounded-full nonSelectable' 
-                                />
-                            </div>
-                            <p className='nonSelectable'>{pokemonInfo.sprites[shownImageIdx].gen}</p>
-                          </div>
-                          <IoCaretForwardOutline onClick={nextImage} size={30} className='hover:scale-125 duration-200 transition-all'/>
-                        </div>
-                      }
-                    </div>
-                    
 
                   {/* other info*/}
-                  <div className='col-span-4 p-4 space-y-4  '>
+                  <div className='md:col-span-4 col-span-8 p-4 space-y-4  '>
                     {/* name and types */}
-                    <div className='text-left space-y-4 nonSelectable'>
-                      <p className='text-3xl font-semibold '>
+                    <div className='text-left md:space-y-4 nonSelectable flex md:block items-center space-x-4 '>
+                      <p className='md:text-3xl text-xl font-semibold '>
                         {pokemonInfo.name}
                       </p>
-                      <div className='w-2/3 flex space-x-4 '>
+                      <div className='md:w-2/3 flex space-x-4 '>
                         {pokemonInfo.types.map((type, index) => (
-                          <div 
-                          key={index}
-                          className='w-fit p-2 rounded-md border-2 border-primaryRed'>
+                          <div key={index} className='w-fit p-2 rounded-md border-2 border-primaryRed'>
                             {type}
                           </div>
                         ))}
@@ -173,7 +173,7 @@ const PokemonInfo: React.FC<PokemonInfoParams> = ({selectedPokemon}) => {
                               <p className='text-xl'>{pokemonInfo.name} abilities:</p>
                               { pokemonInfo.abilities &&
         
-                              <div className="h-[250px] overflow-hidden">
+                              <div className="h-[350px] overflow-hidden">
                               <table className="w-full table-auto">
                                 <thead>
                                   <tr>
@@ -183,7 +183,7 @@ const PokemonInfo: React.FC<PokemonInfoParams> = ({selectedPokemon}) => {
                                 </thead>
                               </table>
                               
-                              <div className="overflow-y-auto h-[calc(250px-36px)]">
+                              <div className="overflow-y-auto h-[calc(350px-36px)]">
                                 <table className="w-full table-auto">
                                   <tbody>
                                     {pokemonInfo.abilities.map((ability, index) => (
@@ -193,10 +193,10 @@ const PokemonInfo: React.FC<PokemonInfoParams> = ({selectedPokemon}) => {
                                     >
                                         <td className="px-4 py-2">{ability.name}</td>
                                         <td className="px-4 py-2">
-                                        {hoveredIndex === index
-                                                ? ability.effect // Show full effect if this row is hovered
-                                                : ability.effect?.substring(0, 100) + '...'} {/* Truncate for non-hovered rows */}
-                                          </td>
+                                          {hoveredIndex === index
+                                              ? ability.effect // Show full effect if this row is hovered
+                                              : ability.effect?.substring(0, 100) + '...'} 
+                                        </td>
                                       </tr>
                                     ))}
                                   </tbody>
